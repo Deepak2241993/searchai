5151a5d8ca9ed32676e734eab8b6519a751b6524
temzsol <sushobhan@thetemz.com>
2024-12-15 21:26:00 +0530

    Updated In Dashboard View Coupon

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -56,32 +56,19 @@
                             More info <i class="bi bi-link-45deg"></i> </a>
                     </div> <!--end::Small Box Widget 3-->
                 </div> <!--end::Col-->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <a href="{{ route('admin.coupon.list') }}">
-                        <div class="info-box mb-3">
-                            <span class="info-box-icon bg-info elevation-1"><i class="nav-icon fa fa-barcode"></i></span>
-
-                            <div class="info-box-content">
-                                <span class="info-box-text"> View Coupon</span>
-                                <span class="info-box-number">{{ $giftCoupon }}</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </a>
-                </div>
-                {{-- <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 4-->
+                
+                <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 4-->
                     <div class="small-box text-bg-danger">
                         <div class="inner">
-                            <h3>65</h3>
-                            <p>Unique Visitors</p>
+                            <h3>{{ $giftCoupon }}</h3>
+                            <p>View Coupon</p>
                         </div> <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                        </svg> <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+                        </svg> <a href="{{ route('admin.coupon.list') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
                             More info <i class="bi bi-link-45deg"></i> </a>
                     </div> <!--end::Small Box Widget 4-->
-                </div> <!--end::Col--> --}}
+                </div> <!--end::Col-->
             </div> <!--end::Row--> <!--begin::Row-->
             {{-- <div class="row"> <!-- Start col -->
                 <div class="col-lg-7 connectedSortable">
