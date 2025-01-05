71d12f3ad17805245ec206abaa3fba1e3ee06ee1
temzsol <sushobhan@thetemz.com>
2025-01-06 05:07:33 +0530

    Updated The Token Table

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Token extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'service_type',
+        'token',
+        'expires_at',
+        'status',
+    ];
 }
