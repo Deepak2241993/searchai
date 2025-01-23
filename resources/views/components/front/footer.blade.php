5d9d97455bdb2e91e42c00c69446f805a6c67ae6
Deepak2241993 <deepakprasad224@gmail.com>
2025-01-23 09:45:33 +0530

    Home Page Footer Updated

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -5,11 +5,11 @@
 			<div class="sm-col-12 md-col-4 md-col lg-col lg-col-4">
 				<h3 style="border-left:3px solid #fff;padding-left:10px">Contact Info</h3>
 				<ul class="list-reset footer-links mb2">
-					<li><a rel="noopener" href="#" target="_blank" class="inline-block footer-link text-decoration-none">INeedAHelp Technologies Pvt. Ltd.</a></li>
-					<li><a rel="noopener" href="#" target="_blank" class="inline-block footer-link text-decoration-none" style="line-height:24px">Defence Colony, New Delhi - 110024, India</a></li>
-					<li class="xs-hide sm-hide"><a class="text-decoration-none footer-link inline-block" href="tel:+#"><i class="fa fa-phone"></i> +91-9811442001</a></li>
+					<li><a rel="noopener" href="#" target="_blank" class="inline-block footer-link text-decoration-none">Navigant Digital Pvt. ltd.</a></li>
+					<li><a rel="noopener" href="#" target="_blank" class="inline-block footer-link text-decoration-none" style="line-height:24px">E-44/3, Pocket D, Okhla Phase II, Okhla Industrial Estate, New Delhi, Delhi 110020</a></li>
+					<li class="xs-hide sm-hide"><a class="text-decoration-none footer-link inline-block" href="tel:+#"><i class="fa fa-phone"></i> +91-93550 93937</a></li>
 					<li class="xs-hide sm-hide"><a class="text-decoration-none footer-link inline-block" href="https://wa.me/#" target="_blank"><i class="fab fa-whatsapp"></i> WhatsApp us</a></li>
-					<li class="xs-hide sm-hide"><a class="text-decoration-none footer-link inline-block" href="mailto:contact@helpersnearme.com"><i class="fa fa-envelope-o"></i> contact@helpersnearme.com</a></li>
+					<li class="xs-hide sm-hide"><a class="text-decoration-none footer-link inline-block" href="mailto:care@searchai.com"><i class="fa fa-envelope-o"></i> care@searchai.com</a></li>
 				</ul>
 				<div class="md-hide lg-hide">
 					<div class="mb1">  
@@ -89,9 +89,9 @@
 				<ul class="list-reset footer-links mb2">
 					<li><a href="about-us.html" class="inline-block footer-link text-decoration-none">About Us</a></li>
 					<li><a href="verifications.html" class="inline-block footer-link text-decoration-none">Verify your Helper, Professionally</a></li>
-					<li><a href="affiliations.html" class="inline-block footer-link text-decoration-none">Affiliations</a></li>
+					{{-- <li><a href="affiliations.html" class="inline-block footer-link text-decoration-none">Affiliations</a></li>
 					<li><a href="customer-reviews.html" class="inline-block footer-link text-decoration-none">Reviews</a></li>
-					<li><a href="refer-a-worker.html" class="inline-block footer-link text-decoration-none">Refer a Worker</a></li>
+					<li><a href="refer-a-worker.html" class="inline-block footer-link text-decoration-none">Refer a Worker</a></li> --}}
 					<li><a target="_blank" href="blog/index.html" class="inline-block footer-link text-decoration-none">Blog</a></li>
 					<li><a target="_blank" href="blog/our-response-to-covid-19/index.html" class="inline-block footer-link text-decoration-none">Our response to Covid-19</a></li>
 				</ul>
@@ -108,7 +108,7 @@
 				</ul>
 			</div>
 			<div class="sm-col-12 md-col-2 md-col lg-col lg-col-2 text-center">
-				<amp-img layout="fixed" width="175" height="175" src="{{url('/front-assets')}}/images/footer_logo.png" alt="Helpers near me"></amp-img>
+				<amp-img layout="fixed" width="150" height="50" src="{{url('/front-assets')}}/images/footer_logo.png" alt="Helpers near me"></amp-img>
 			</div>
 		</nav>
 	  </div>	
@@ -120,7 +120,7 @@
           <a class="social-link" rel="noopener" href="#" target="_blank" aria-label="Helper Near Me on Facebook" style="background:#3b5998"><amp-img layout="fixed" width="40" height="40" src="{{url('/front-assets')}}/images/icons/facebook.svg" alt="Helper Near Me on Facebook"></amp-img></a>
         </li>
         <li class="social-list-item">
-          <a class="social-link" rel="noopener" href="#" target="_blank" aria-label="Helper Near Me on Twitter" style="background:#00aced"><amp-img layout="fixed" width="40" height="40" src="icons/twitter-x.png" alt="Helper Near Me on Twitter"></amp-img></a>
+          <a class="social-link" rel="noopener" href="#" target="_blank" aria-label="Helper Near Me on Twitter" style="background:#00aced"><amp-img layout="fixed" width="40" height="40" src="{{url('/front-assets')}}/images/icons/twitter-x.png" alt="Helper Near Me on Twitter"></amp-img></a>
         </li>
         <li class="social-list-item">
           <a class="social-link" rel="noopener" href="#" target="_blank" aria-label="Helper Near Me on Youtube" style="background:#bb0000"><amp-img layout="fixed" width="40" height="40" src="{{url('/front-assets')}}/images/icons/youtube.svg" alt="Helper Near Me on Youtube"></amp-img></a>
@@ -130,6 +130,6 @@
         </li>
 		  </ul>
 		</nav>
-		<h5 class="text-center">Copyright &copy; 2016-24 INeedAHelp Technologies Pvt. Ltd. | All rights reserved</h5>
+		<h5 class="text-center">Copyright &copy; {{date('Y')}} Navigant Digital Pvt ltd. | All rights reserved | <a href="https://www.thetemz.com/" target="_blank">Develop by TEMZ</a></h5>
 	</div>
 </footer>
